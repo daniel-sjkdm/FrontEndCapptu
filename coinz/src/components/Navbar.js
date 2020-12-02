1896955c97ae6b6336a921539b2a9feb9135acf0
s_j_u_k_d_o_m <daniel.py.216@gmail.com>
2020-12-02 16:26:18 -0600

    Navbar bitcoinz

---
+++ coinz/src/components/Navbar.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { 
+	AppBar,
+	Toolbar,
+	Typography,
+	IconButton
+} from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+
+
+const useStyles = makeStyles(theme => ({
+	main__appbar: {
+		backgroundColor: "#272727",
+		display: "flex",
+		flexDirection: "row",
+		justifyContent: "center"
+	},
+	icon: {
+		color: "white"
+	}
+}));
+
+
+const Navbar = (props) => {
+	const classes = useStyles();
+	return (
+		<AppBar
+			color="primary"
+			position="fixed"
+			className={classes.main__appbar}
+		>
+			<Toolbar>
+				<IconButton>
+					<i className="fab fa-bitcoin fa-2x icon"></i>
+				</IconButton>
+				<Typography variant="h5">
+					Bitcoinz
+				</Typography>
+			</Toolbar>
+		</AppBar>
+	)
+};
+
+
+export default Navbar;
