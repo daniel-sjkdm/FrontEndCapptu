7c68262eecd3cbbcf0c4ce28fd2a83b19c89084e
s_j_u_k_d_o_m <daniel.py.216@gmail.com>
2020-12-04 22:05:07 -0600

    Added column's name prop to avoid a warning

--- coinz/src/components/Tablecoinz.js
+++ coinz/src/components/Tablecoinz.js
@@ -8,22 +8,27 @@ import MUIDataTable from 'mui-datatables';
 
 const columns = [
     {
+        name: "book",
         label: "Book", 
         options: { filter: false, sort: false }
     },
     {
+        name: "created_at",
         label: "Created at", 
         options: { filter: false, sort: false }
     },
-    {
+    {  
+        name: "volume",
         label: "Volume",
         options: { filter: false, sort: false }
     }, 
     {
+        name: "high",
         label: "High",
         options: { filter: false, sort: false }
     },
     { 
+        name: "low",
         label: "Low",
         options: { filter: false, sort: false }
     }
