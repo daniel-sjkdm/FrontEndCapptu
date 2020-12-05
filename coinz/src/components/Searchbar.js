7617b4494bc309f576dd89108c244c013b2dbdd6
s_j_u_k_d_o_m <daniel.py.216@gmail.com>
2020-12-04 18:23:01 -0600

    Searchbar component

--- coinz/src/components/Searchbar.js
+++ coinz/src/components/Searchbar.js
@@ -1,54 +1,128 @@
 import React, { useState } from 'react';
 import {
-	TextField,
-	Button
+	Button,
+	InputAdornment,
+	Input,
+	InputLabel,
+	Typography
 } from '@material-ui/core';
 import {
 	makeStyles 
 } from '@material-ui/core/styles';
+import {
+	Alert
+} from '@material-ui/lab';
+import SearchIcon from '@material-ui/icons/Search';
 import axios from 'axios';
 
 
 
 const useStyles = makeStyles(theme => ({
 	root: {
-		marginTop: "30px",
+		marginTop: "40px",
 		fontSize: "14px",
+	},
+	alert: {
+		marginBottom: "25px"
+	},
+	text: {
+		fontFamily: "Roboto Mono, monospace",
 	}
 }));
 
 
-const handleSubmit = async query => {
-	const url = "https://api.bitso.com/v3/trades/?book=btc_mxn"
-	
-	try {
-		const response = await axios({
-			method: "GET",
-			url: url
-		});
-		const json = response.json();
-		console.log(json);
-	}
-	catch {
-		alert("Error");
-	}
+
+const validateDate = date => {
+	// if (date.match(/\d{4}-\d{2}-\d{2}/g)) {
+	// 	console.log("valid")
+	// 	return true;
+	// }
+	// else {
+	// 	console.log("invalid")
+	// 	return false;
+	// }
+	return true
 };
 
 
+const Searchbar = ({setData}) => {
 
-const Searchbar = () => {
-	const classes = useStyles();
 	const [query, setQuery] = useState("");
+	const [error, setError] = useState(false);
+	const [errorMessage, setErrorMessage] = useState("");
+	const classes = useStyles();
+
+	const handleSubmit = query => {
+		const date_array = query.split("to");
+		const url = `http://localhost:8000/coinz/api/date-range/?date1=${date_array[0]}&date2=${date_array[1]}`;
+	
+
+		axios.get(url)
+		.then(
+			response => {
+				setData(response.data)
+			}
+		)
+		.catch(
+			error => {
+				console.log(error)
+			}
+		)
+
+	};
+
 	return (
 		<div className={classes.root}>
-			<TextField
-				value={query}
-				onChange={(e) => setQuery(e.target.value)}
-			/>
+			{
+				error? ( 
+					<Alert 
+						className={classes.alert}
+						severity="warning"
+						onClose={() => setError(false)}
+					> 
+						{ errorMessage } 
+					</Alert> 
+				) : ""
+			}
+			<Typography className={classes.text} variant="body1">
+				You can get the price of the bitcoin converted to mxn currency. <br></br>
+				Use the search bar and the buttons to filter 
+				the content based on your interests. <br></br> <br></br>
+			</Typography>
+			<Typography className={classes.text} variant="body1">
+				You can search by date: yyyy/mm/dd <br></br>
+				Or search by date range: yyyy/mm/dd - yyyy/mm/dd
+			</Typography>
+			<InputLabel 
+				style={{ marginTop: "25px" }} 
+				htmlFor="coinz__input"
+				className={classes.text}
+			> 
+				Seach by date 
+			</InputLabel>
+				<Input
+					id="coinz__input"
+					value={query}
+					onChange={(e) => setQuery(e.target.value)}
+					startAdornment={
+						<InputAdornment position="start">
+							<SearchIcon />
+						</InputAdornment>
+					}
+				/>
 			<Button
 				color="primary"
 				variant="contained"
-				onClick={() => query? handleSubmit(query) : alert("Input can't be empty")}
+				size="small"
+				onClick={() => query? ( 
+					validateDate(query)? handleSubmit(query) : (
+						setError(true),
+						setErrorMessage("Enter a valid date: yyyy/mm/dd or yyyy/mm/dd to yyyy/mm/dd")
+					)
+				) : (
+					setError(true),
+					setErrorMessage("The query can't be empty")
+				)}
 			>
 				Search
 			</Button>
