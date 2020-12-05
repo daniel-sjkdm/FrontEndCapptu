574926cf88636b4a6e054bccb82765ae05e19b64
s_j_u_k_d_o_m <daniel.py.216@gmail.com>
2020-12-04 18:15:31 -0600

    App layout finished

--- coinz/src/App.js
+++ coinz/src/App.js
@@ -1,15 +1,36 @@
-import logo from './logo.svg';
 import Navbar from './components/Navbar';
-import Searchbar from './components/Searchbar';
+// import Searchbar from './components/Searchbar';
+import Controls from './components/Controls';
+import Tablecoinz from './components/Tablecoinz';
+import Coin from './components/Coin';
+import {
+  Container,
+  Typography
+} from '@material-ui/core';
 import './App.css';
+import { useState } from 'react';
 
 
 
 function App() {
+  const [ data , setData ] = useState("");
   return (
     <div className="App">
 			<Navbar />
-			<Searchbar />
+      <Container>
+        <Typography 
+          style={{ 
+            fontFamily: "Roboto Mono, monospace",
+            marginTop: "30px"
+          }} 
+          variant="h3"
+        >
+          BTC 2 MXN
+        </Typography>
+        <Coin />
+        <Controls setData={setData}/>
+        <Tablecoinz data={data}/>
+      </Container>
     </div>
   );
 }
